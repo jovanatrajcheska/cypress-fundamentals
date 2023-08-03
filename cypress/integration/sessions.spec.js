8fabc1fa8edd9778663d506957cde20be05a6091
jovanatrajcheska <trajceskaj55@gmail.com>
2023-08-03 14:39:01 +0200

    Add aliases and GraphQL Intercept

--- cypress/integration/sessions.spec.js
+++ cypress/integration/sessions.spec.js
@@ -1,10 +1,18 @@
 /// <reference types="cypress" />
 
 describe("Sessions", () => {
+    beforeEach(() => {
+        cy.visit("/conference");
+        cy.get("h1").contains("View Sessions").click();
+        cy.url().should("include", "/sessions");
+
+        // Add alias for buttons
+        cy.get("[data-cy=Wednesday]").as("WednesdayButton");
+        cy.get("[data-cy=Thursday]").as("ThursdayButton");
+        cy.get("[data-cy=Friday]").as("FridayButton");
+        cy.get("[data-cy=AllSessions]").as("AllSessionsButton");
+    });
   it("should navigate to conference sessions page and view day filter buttons", () => {
-    cy.visit("/conference");
-    cy.get("h1").contains("View Sessions").click();
-    cy.url().should("include", "/sessions");
 
     // Validate day filter buttons exist
     cy.get("[data-cy=AllSessions]");
@@ -14,9 +22,13 @@ describe("Sessions", () => {
   });
 
   it("should filter sessions and display only Thursday sessions when Thursday button is clicked", () => {
-    cy.get("[data-cy=Thursday]").click();
+
+    cy.intercept("POST", "http://localhost:4000/graphql").as("getSessionInfo");
+    cy.get("@ThursdayButton").click();
+    cy.wait("@getSessionInfo");
 
     // Validate Thursday sessions are visible
+    cy.get("@ThursdayButton").click();
     cy.get("[data-cy=day]").contains("Thursday").should("be.visible");
 
     // Validate other days are not visible
@@ -25,7 +37,10 @@ describe("Sessions", () => {
   });
 
   it("should filter sessions and display only Friday sessions when Friday button is clicked", () => {
-    cy.get("[data-cy=Friday]").click();
+
+    cy.intercept("POST", "http://localhost:4000/graphql").as("getSessionInfo");
+    cy.get("@FridayButton").click();
+    cy.wait("@getSessionInfo");
 
     // Validate Friday sessions are visible
     cy.get("[data-cy=day]").contains("Friday").should("be.visible");
@@ -36,9 +51,9 @@ describe("Sessions", () => {
   });
 
   it("should filter sessions and display only Wednesday sessions when Wednesday button is clicked", () => {
-    cy.get("[data-cy=Wednesday]").click();
 
     // Validate Wednesday sessions are visible
+    cy.get("@WednesdayButton").click();
     cy.get("[data-cy=day]").contains("Wednesday").should("be.visible");
 
     // Validate other days are not visible
@@ -47,7 +62,10 @@ describe("Sessions", () => {
   });
 
   it("should filter sessions and display all sessions when All Sessions button is clicked", () => {
-    cy.get("[data-cy=AllSessions]").click();
+    
+    cy.intercept("POST", "http://localhost:4000/graphql").as("getSessionInfo");
+    cy.get("@AllSessionsButton").click();
+    cy.wait("@getSessionInfo");
 
     // Validate all sessions are visible
     cy.get("[data-cy=day]").contains("Wednesday").should("be.visible");
